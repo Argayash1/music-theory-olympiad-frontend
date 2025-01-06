1094d1744ae9363e3943f97500f5868b5002d28e
Argayash <zilberman.yakoff@yandex.ru>
2025-01-06 12:33:45 +0500

    chore: в компоненте BrandBanner обновлён URL для кнопки Участвовать

--- src/components/BrandBanner/index.tsx
+++ src/components/BrandBanner/index.tsx
@@ -3,6 +3,7 @@ import { BrandBannerSkeleton, CTA, Logo, LogoContainer, MainInfo } from '../../c
 import styles from './BrandBanner.module.scss';
 import { useSelector } from 'react-redux';
 import { selectOlympData } from '../../redux/olympData/selectors';
+import { PARTICIPATION_URL } from '../../utils/constants';
 
 export const BrandBanner = React.forwardRef<HTMLElement>((props, ref) => {
   const { items, status } = useSelector(selectOlympData);
@@ -19,10 +20,7 @@ export const BrandBanner = React.forwardRef<HTMLElement>((props, ref) => {
             музыкально-теоретическим предметам им.&nbsp;З.&nbsp;А.&nbsp;Визеля
           </h1>
           <MainInfo dates={items[0].dates} registrationDates={items[0].registrationDates} city={items[0].city} />
-          <CTA
-            path='https://docs.google.com/forms/d/e/1FAIpQLSf9TSakRFx8GSTfv1A53a4aQjkR-CW6NDznfeEhQQ-mgn8VOw/viewform'
-            type='brand-bunner'
-          />
+          <CTA path={PARTICIPATION_URL} type='brand-bunner' />
           <Logo place='brand-bunner' />
         </div>
       )}
